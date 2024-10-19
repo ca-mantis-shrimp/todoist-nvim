9135f08910271d934e6bee3a4a9d534aede252d5
Darrion Burgess <dargondab9@gmail.com>
2024-10-19 05:33:51 -0500

    i did it! not only were we able to get the function working to add other lines, but i think this is the cleanest iteration of this that i have ever seen by a mile!

---
+++ lua/Todoist/model/new_transformations.fnl
@@ -0,0 +1,43 @@
+(local M {})
+
+(fn M.add_root_depth [project]
+  (tset project :depth 0)
+  project)
+
+(fn M.is_root_project [project]
+  (or (= project.parent_id nil) (= project.parent_id _G.vim.NIL)))
+
+(fn M.is_higher_child_order [type_1 type_2]
+  (< type_1.child_order type_2.child_order))
+
+(fn M.get_project_str [project]
+  (.. (_G.string.rep "#" (+ project.depth 1)) " " project.name "|>" project.id))
+
+(fn M.get_comment_str [cmnt]
+  (.. "+ " cmnt.content "|>" cmnt.id))
+
+(fn M.get_section_str [section]
+  (.. "& " section.name "|>" section.id))
+
+(fn M.list_is_populated [list]
+  (and list (> (length list) 0)))
+
+(fn M.append_list_lines [lines list str_generator]
+  (table.insert lines
+                (_G.unpack (icollect [_ value (ipairs list)]
+                             (str_generator value)))))
+
+(fn M.get_project_lines [project]
+  (local lines [(M.get_project_str project)])
+  (when (M.list_is_populated project.comments)
+    (M.append_list_lines lines project.comments M.get_comment_str))
+  (when (M.list_is_populated project.sections)
+    (M.append_list_lines lines project.sections M.get_section_str))
+  (when (M.list_is_populated project.children)
+    (M.append_list_lines lines project.children M.add_project_lines))
+  lines)
+
+(M.add_project_lines {:comments nil :depth 0 :name :test-proj :id 2421})
+
+M
+
---
+++ lua/Todoist/model/new_transformations.lua
@@ -0,0 +1,58 @@
+-- [nfnl] Compiled from lua/Todoist/model/new_transformations.fnl by https://github.com/Olical/nfnl, do not edit.
+local M = {}
+M.add_root_depth = function(project)
+  project["depth"] = 0
+  return project
+end
+M.is_root_project = function(project)
+  return ((project.parent_id == nil) or (project.parent_id == _G.vim.NIL))
+end
+M.is_higher_child_order = function(type_1, type_2)
+  return (type_1.child_order < type_2.child_order)
+end
+M.get_project_str = function(project)
+  return (_G.string.rep("#", (project.depth + 1)) .. " " .. project.name .. "|>" .. project.id)
+end
+M.get_comment_str = function(cmnt)
+  return ("+ " .. cmnt.content .. "|>" .. cmnt.id)
+end
+M.get_section_str = function(section)
+  return ("& " .. section.name .. "|>" .. section.id)
+end
+M.list_is_populated = function(list)
+  return (list and (#list > 0))
+end
+M.append_list_lines = function(lines, list, str_generator)
+  local function _1_()
+    local tbl_21_auto = {}
+    local i_22_auto = 0
+    for _, value in ipairs(list) do
+      local val_23_auto = str_generator(value)
+      if (nil ~= val_23_auto) then
+        i_22_auto = (i_22_auto + 1)
+        tbl_21_auto[i_22_auto] = val_23_auto
+      else
+      end
+    end
+    return tbl_21_auto
+  end
+  return table.insert(lines, _G.unpack(_1_()))
+end
+M.get_project_lines = function(project)
+  local lines = {M.get_project_str(project)}
+  if M.list_is_populated(project.comments) then
+    M.append_list_lines(lines, project.comments, M.get_comment_str)
+  else
+  end
+  if M.list_is_populated(project.sections) then
+    M.append_list_lines(lines, project.sections, M.get_section_str)
+  else
+  end
+  if M.list_is_populated(project.children) then
+    M.append_list_lines(lines, project.children, M.add_project_lines)
+  else
+  end
+  return lines
+end
+M.add_project_lines({comments = nil, depth = 0, name = "test-proj", id = 2421})
+return M
