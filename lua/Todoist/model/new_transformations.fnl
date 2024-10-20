95dc8b1830f91b8edfd6ba4118b46cceeef6cc96
Darrion Burgess <dargondab9@gmail.com>
2024-10-19 22:41:29 -0700

    added a bunch of functionality to the transformations file

--- lua/Todoist/model/new_transformations.fnl
+++ lua/Todoist/model/new_transformations.fnl
@@ -7,7 +7,23 @@
 
 (fn M.is_root_project [project]
   "checks if a project is or is not a root project by checking depth"
-  (or project.parent_id (= project.parent_id _G.vim.NIL)))
+  (or (not project.parent_id) (= project.parent_id _G.vim.NIL)))
+
+(fn M.is_project_comment [project todoist_comment]
+  "checks if a comment belongs to a project"
+  (= project.id todoist_comment.project_id))
+
+(fn M.is_project_section [project section]
+  "checks if a section belongs to a project"
+  (= project.id section.project_id))
+
+(fn M.is_child_project [project potential_child]
+  "checks if a project is a child of another project"
+  (= project.id potential_child.parent_id))
+
+(fn M.is_parent_project [project potential_parent]
+  "checks if a project is a child of another project"
+  (= project.parent_id potential_parent.id))
 
 (fn M.is_higher_child_order [type_1 type_2]
   "checks if the first dictionary has a lower child order than the second"
@@ -29,22 +45,71 @@
   "returns true if the collection is both non-nil and has atleast one item in it"
   (and list (> (length list) 0)))
 
+(fn M.add_list_to_project [project list_name list checker]
+  "higher order function that takes a project, a list name, a list, and a checker function, and adds the value to the project if the checker function returns true"
+  (when (M.list_is_populated list)
+    (tset project list_name (icollect [_ value (ipairs list)]
+                              (when (checker project value)
+                                value)))))
+
+(fn M.add_lists_to_projects [opts]
+  "add children stuff to project list"
+  (each [_ project (ipairs opts.response.projects)]
+    (M.add_list_to_project project :children opts.response.projects
+                           ;We can assume projects are there
+                           M.is_project_child)
+    (M.add_list_to_project project :comments (?. opts :response :comments)
+                           ;comments and sections however we cannot assume are there
+                           M.is_project_comment)
+    (M.add_list_to_project project :sections (?. opts :response :sections)
+                           M.is_project_section))
+  opts)
+
+(fn M.add_root_project_list [opts]
+  "add new 'root_projects' key that will actually return the structure of the work"
+  (tset opts :root_projects (icollect [_ project (ipairs opts.response.projects)]
+                              (when (M.is_root_project project)
+                                project)))
+  opts)
+
+(fn M.add_depth_to_root_project [project projects]
+  "add the depth for the root project and all child projects (recursively) if necessary"
+  (tset project :depth (or (+ (. (. (icollect [_ potential_parent (ipairs projects)]
+                                      (when (M.is_project_parent project
+                                                                 potential_parent)
+                                        potential_parent))
+                                    1)) :depth) 1) 0)
+  (when (M.list_is_populated project.children)
+    (each [_ child (ipairs project.children)]
+      (M.add_depth_to_project child projects)))
+  project)
+
+(fn M.add_depth_to_root_projects [opts]
+  "add the depth to all root projects and their children recursively"
+  (each [_ project (ipairs opts.response.root_projects)]
+    (M.add_depth_to_root_project project opts.response.projects)))
+
 (fn M.append_list_lines [lines list str_generator]
-  "given base list of strings, a list of dictionaries to be converted, and a function to convert each dictionary, append the result of converting the list to the original lines"
-  (table.insert lines
-                (_G.unpack (icollect [_ value (ipairs list)]
-                             (str_generator value)))))
+  "given base-list of strings, a list of dictionaries to be converted, and a function to convert each dictionary, append the result of converting the list to the original lines"
+  (when (M.list_is_populated list)
+    (table.insert lines
+                  (_G.unpack (icollect [_ value (ipairs list)]
+                               (str_generator value))))))
 
 (fn M.get_project_lines [project]
   "grabs the list of string lines for the project, as well as all comments, sections, and child projects recursively and returns the list"
   (let [lines [(M.get_project_str project)]]
-    (when (M.list_is_populated project.comments)
-      (M.append_list_lines lines project.comments M.get_comment_str))
-    (when (M.list_is_populated project.sections)
-      (M.append_list_lines lines project.sections M.get_section_str))
-    (when (M.list_is_populated project.children)
-      (M.append_list_lines lines project.children M.add_project_lines))
+    (M.append_list_lines lines project.sections M.get_section_str)
+    (M.append_list_lines lines project.comments M.get_comment_str)
+    (M.append_list_lines lines project.children M.add_project_lines)
     lines))
+
+(fn M.add_todoist_lines [opts]
+  "adds all lines from the list of root projects to the new 'lines' key"
+  (tset opts :lines
+        (icollect [_ project (ipairs opts.response.root_projects)]
+          (_G.unpack (M.get_project_lines project))))
+  opts)
 
 M
 
--- lua/Todoist/model/new_transformations.lua
+++ lua/Todoist/model/new_transformations.lua
@@ -5,7 +5,19 @@ M.add_root_depth = function(project)
   return project
 end
 M.is_root_project = function(project)
-  return (project.parent_id or (project.parent_id == _G.vim.NIL))
+  return (not project.parent_id or (project.parent_id == _G.vim.NIL))
+end
+M.is_project_comment = function(project, todoist_comment)
+  return (project.id == todoist_comment.project_id)
+end
+M.is_project_section = function(project, section)
+  return (project.id == section.project_id)
+end
+M.is_child_project = function(project, potential_child)
+  return (project.id == potential_child.parent_id)
+end
+M.is_parent_project = function(project, potential_parent)
+  return (project.parent_id == potential_parent.id)
 end
 M.is_higher_child_order = function(type_1, type_2)
   return (type_1.child_order < type_2.child_order)
@@ -22,36 +34,168 @@ end
 M.list_is_populated = function(list)
   return (list and (#list > 0))
 end
-M.append_list_lines = function(lines, list, str_generator)
-  local function _1_()
+M.add_list_to_project = function(project, list_name, list, checker)
+  if M.list_is_populated(list) then
+    local _1_
+    do
+      local tbl_21_auto = {}
+      local i_22_auto = 0
+      for _, value in ipairs(list) do
+        local val_23_auto
+        if checker(project, value) then
+          val_23_auto = value
+        else
+          val_23_auto = nil
+        end
+        if (nil ~= val_23_auto) then
+          i_22_auto = (i_22_auto + 1)
+          tbl_21_auto[i_22_auto] = val_23_auto
+        else
+        end
+      end
+      _1_ = tbl_21_auto
+    end
+    project[list_name] = _1_
+    return nil
+  else
+    return nil
+  end
+end
+M.add_lists_to_projects = function(opts)
+  for _, project in ipairs(opts.response.projects) do
+    M.add_list_to_project(project, "children", opts.response.projects, M.is_project_child)
+    local _6_
+    do
+      local t_5_ = opts
+      if (nil ~= t_5_) then
+        t_5_ = t_5_.response
+      else
+      end
+      if (nil ~= t_5_) then
+        t_5_ = t_5_.comments
+      else
+      end
+      _6_ = t_5_
+    end
+    M.add_list_to_project(project, "comments", _6_, M.is_project_comment)
+    local _10_
+    do
+      local t_9_ = opts
+      if (nil ~= t_9_) then
+        t_9_ = t_9_.response
+      else
+      end
+      if (nil ~= t_9_) then
+        t_9_ = t_9_.sections
+      else
+      end
+      _10_ = t_9_
+    end
+    M.add_list_to_project(project, "sections", _10_, M.is_project_section)
+  end
+  return opts
+end
+M.add_root_project_list = function(opts)
+  local _13_
+  do
     local tbl_21_auto = {}
     local i_22_auto = 0
-    for _, value in ipairs(list) do
-      local val_23_auto = str_generator(value)
+    for _, project in ipairs(opts.response.projects) do
+      local val_23_auto
+      if M.is_root_project(project) then
+        val_23_auto = project
+      else
+        val_23_auto = nil
+      end
       if (nil ~= val_23_auto) then
         i_22_auto = (i_22_auto + 1)
         tbl_21_auto[i_22_auto] = val_23_auto
       else
       end
     end
-    return tbl_21_auto
+    _13_ = tbl_21_auto
   end
-  return table.insert(lines, _G.unpack(_1_()))
+  opts["root_projects"] = _13_
+  return opts
+end
+M.add_depth_to_root_project = function(project, projects)
+  local _16_
+  do
+    local tbl_21_auto = {}
+    local i_22_auto = 0
+    for _, potential_parent in ipairs(projects) do
+      local val_23_auto
+      if M.is_project_parent(project, potential_parent) then
+        val_23_auto = potential_parent
+      else
+        val_23_auto = nil
+      end
+      if (nil ~= val_23_auto) then
+        i_22_auto = (i_22_auto + 1)
+        tbl_21_auto[i_22_auto] = val_23_auto
+      else
+      end
+    end
+    _16_ = tbl_21_auto
+  end
+  project["depth"][((_16_[1] + "depth") or 1)] = 0
+  if M.list_is_populated(project.children) then
+    for _, child in ipairs(project.children) do
+      M.add_depth_to_project(child, projects)
+    end
+  else
+  end
+  return project
+end
+M.add_depth_to_root_projects = function(opts)
+  for _, project in ipairs(opts.response.root_projects) do
+    M.add_depth_to_root_project(project, opts.response.projects)
+  end
+  return nil
+end
+M.append_list_lines = function(lines, list, str_generator)
+  if M.list_is_populated(list) then
+    local function _20_()
+      local tbl_21_auto = {}
+      local i_22_auto = 0
+      for _, value in ipairs(list) do
+        local val_23_auto = str_generator(value)
+        if (nil ~= val_23_auto) then
+          i_22_auto = (i_22_auto + 1)
+          tbl_21_auto[i_22_auto] = val_23_auto
+        else
+        end
+      end
+      return tbl_21_auto
+    end
+    return table.insert(lines, _G.unpack(_20_()))
+  else
+    return nil
+  end
 end
 M.get_project_lines = function(project)
   local lines = {M.get_project_str(project)}
-  if M.list_is_populated(project.comments) then
-    M.append_list_lines(lines, project.comments, M.get_comment_str)
-  else
-  end
-  if M.list_is_populated(project.sections) then
-    M.append_list_lines(lines, project.sections, M.get_section_str)
-  else
-  end
-  if M.list_is_populated(project.children) then
-    M.append_list_lines(lines, project.children, M.add_project_lines)
-  else
-  end
+  M.append_list_lines(lines, project.sections, M.get_section_str)
+  M.append_list_lines(lines, project.comments, M.get_comment_str)
+  M.append_list_lines(lines, project.children, M.add_project_lines)
   return lines
+end
+M.add_todoist_lines = function(opts)
+  local _23_
+  do
+    local tbl_21_auto = {}
+    local i_22_auto = 0
+    for _, project in ipairs(opts.response.root_projects) do
+      local val_23_auto = _G.unpack(M.get_project_lines(project))
+      if (nil ~= val_23_auto) then
+        i_22_auto = (i_22_auto + 1)
+        tbl_21_auto[i_22_auto] = val_23_auto
+      else
+      end
+    end
+    _23_ = tbl_21_auto
+  end
+  opts["lines"] = _23_
+  return opts
 end
 return M
